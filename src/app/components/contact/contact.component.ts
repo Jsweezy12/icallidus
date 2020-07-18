8828a2a6587f47d4ea1e4b084f9629bb35d3f76a
John Swann <john.swann@aurotechcorp.com>
2020-07-18 08:29:55 -0400

    started to embed html

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -14,40 +14,31 @@ export class ContactComponent implements OnInit {
   html;
   prev;
   prevName;
-  data={CN:''}
+  data={CN: "",
+  POC: "",
+  email: "",
+  notes: ""}
 ngOnInit() {
   this.prevName = localStorage.getItem("prev");
   this.prev = localStorage.getItem("prevRoute");
-  
-    this.connector.allhtml.subscribe(res=>{
-      this.res = res
-      setTimeout(() => {
-        this.html=this.dom.bypassSecurityTrustHtml(res[0].htmlversion20)
-      }, 1500);
-      setTimeout(() => {
-        document.querySelectorAll('#send').forEach(elem=>{
-          elem = elem as HTMLDivElement;
-          elem.addEventListener('click',()=>{
-            this.getdata();
-           })
-        })
-      }, 2000);
-     
-    });
- 
-     
+    setTimeout(() => {
+      document.querySelectorAll('#send').forEach(elem=>{
+        elem = elem as HTMLDivElement;
+        elem.addEventListener('click',()=>{
+          this.getdata();
+         })
+      })
+    }, 2000);
     
     
   }
 
   getdata(){
-    let r = new Function(this.res[0].functions.getFormData.arguments,this.res[0].functions.getFormData.functionbody);
-    let data = r();
     console.log(this.data)
     let message = `
-    ${data.POC} from ${data.CN} is requesting a return response to eamail ${data.email} regarding the following,
+    ${this.data.POC} from ${this.data.CN} is requesting a return response to eamail ${this.data.email} regarding the following,
  
-    "${data.notes}"
+    "${this.data.notes}"
     
     `
     console.log(message)
